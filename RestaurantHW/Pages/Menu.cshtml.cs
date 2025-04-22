9078dea5b405daa128c5d085bf46bd2f63f4092c
Mikhail Sherkunkov <sherkunkov@gmail.com>
2025-04-22 21:44:15 +0800

    Add project files.

---
+++ RestaurantHW/Pages/Menu.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RestaurantHW.Pages
+{
+    public class MenuModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
